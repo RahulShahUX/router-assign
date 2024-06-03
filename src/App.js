6e9762c9a5c7b897eb342cff89e7c5ba82400833
Rahul <rn.007.shah@gmail.com>
2024-06-03 21:37:35 +0530

    router lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import './App.css';
+import React, { Suspense } from 'react';
 import UseStateHook from './Components/UseStateHook/UseStateHook';
-import UseEffectHook from './Components/UseEffectHook/UseEffectHook';
+// import UseEffectHook from './Components/UseEffectHook/UseEffectHook';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import { Container } from 'react-bootstrap';
 import Home from './Components/Home/Home';
 import Routerpage from './Components/Routerpage/Routerpage';
 import Hookspage from './Components/Hookspage/Hookspage';
+const UseEffectHook = React.lazy(() => import('./Components/UseEffectHook/UseEffectHook'));
+
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      {/* <div className='loader'>Loading</div> */}
       <Container fluid className='py-3'>
-        <Routes>
-          <Route element={<Home/>} path='home' />
-          <Route element={<Routerpage />} path='router' />
-          <Route element={<Hookspage />} path='hooks'>
-            <Route element={<UseStateHook />} path='usestate' />
-            <Route element={<UseEffectHook />} path='useeffect' />
-          </Route>
-        </Routes>
+        <Suspense fallback={<div className='loader'><span>Loading</span></div>}>
+          <Routes>
+            <Route element={<Home />} path='home' />
+            <Route element={<Routerpage />} path='router' />
+            <Route element={<Hookspage />} path='hooks'>
+              <Route element={<UseStateHook />} path='usestate' />
+              <Route element={<UseEffectHook />} path='useeffect' />
+            </Route>
+          </Routes>
+        </Suspense>
       </Container>
     </BrowserRouter>
   );
